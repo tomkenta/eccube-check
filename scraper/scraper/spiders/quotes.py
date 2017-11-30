38f26d85dc74cb5d0110c7c518f418f9a2699554
横田 健太 <k.yokota@MAC0011.local>
2017-11-30 16:59:05 +0900

    for 1 time scrape

--- scraper/scraper/spiders/quotes.py
+++ scraper/scraper/spiders/quotes.py
@@ -10,14 +10,27 @@ class QuotesSpider(CrawlSpider):
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
-    # rules = (
-    #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    # )
+    rules = (
+        Rule(LinkExtractor(allow=r'.*'),
+             callback='parse_start_url',
+             follow=True,
+             ),
+    )
 
-    def parse(self, response):
-        for quote_html in response.css('div.quote'):
+    def parse_start_url(self, response):
+        """"start_urlsのインデックスページもスクレイピングする"""
+        return self.parse_item(response)
+
+    def parse_item(self, response):
+        """"クロールしたページからItemをスクレイピングする"""
+        # 1ページにつき1アイテムのみ
+        items = []
+        for i, quote_html in enumerate(response.css('div.quote')):
+            if i > 1:
+                return items
+
             item = Quote()
             item['author'] = quote_html.css('small.author::text').extract_first()
             item['text'] = quote_html.css('span.text::text').extract_first()
             item['tags'] = quote_html.css('div.tags a.tag::text').extract()
-            yield item
+            items.append(item)
