92e7c172e0ad7ad8526e0be54721b7b5582c9aaa
k.yokota <kyokota@netprotections.co.jp>
2018-01-30 11:08:04 +0900

    [MOD] Loggingの重複の解消

--- scraper/scraper/spiders/sources.py
+++ scraper/scraper/spiders/sources.py
@@ -17,16 +17,14 @@ logger.setLevel(DEBUG)
 # コンソール表示用
 stream_handler = StreamHandler()
 # ログファイル用
-file_handler = FileHandler(filename='log/test.log')
+
 
 formatter = Formatter(
     fmt="%(asctime)s [%(name)s Line:%(lineno)d] %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(DEBUG)
-file_handler.setFormatter(formatter)
-file_handler.setLevel(DEBUG)
+
 logger.addHandler(stream_handler)
-logger.addHandler(file_handler)
 
 
 class SourcesSpider(CrawlSpider):
